1a58c98775c4d4ddbd6af0cbd6cfb73f591650ba
sairanj <ranjith.rajagopal4u@gmail.com>
2022-01-03 20:12:17 -0500

    rename the item master constraint

--- SaiERP_Database_Project/Inventory/Tables/Item_Group.sql
+++ SaiERP_Database_Project/Inventory/Tables/Item_Group.sql
@@ -1,5 +1,5 @@
 ﻿CREATE TABLE [Inventory].[Item_Group] (
-    [Group_No]            UNIQUEIDENTIFIER NOT NULL,
+    [Group_Id]            UNIQUEIDENTIFIER NOT NULL,
     [Group_Code]          INT              NOT NULL,
     [Sub_Group_Code]      INT              NOT NULL,
     [Group_Code_Name]     VARCHAR (1000)   NOT NULL,
@@ -8,7 +8,7 @@
     [LastModified_Date]   DATE             NOT NULL,
     [Created_By]          BIGINT           NULL,
     [LastModified_By]     BIGINT           NULL,
-    PRIMARY KEY CLUSTERED ([Group_No] ASC),
+    PRIMARY KEY CLUSTERED ([Group_Id] ASC),
     FOREIGN KEY ([Created_By]) REFERENCES [Hrms].[Employee_Master] ([Employee_Id]),
     FOREIGN KEY ([LastModified_By]) REFERENCES [Hrms].[Employee_Master] ([Employee_Id])
 );
