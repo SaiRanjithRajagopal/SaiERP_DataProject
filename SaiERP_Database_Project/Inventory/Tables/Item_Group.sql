3ba02139ec55af09941c8c078aab0b6262eca6f8
sairanj <ranjith.rajagopal4u@gmail.com>
2022-01-04 13:40:33 -0500

    modified the constraint

--- SaiERP_Database_Project/Inventory/Tables/Item_Group.sql
+++ SaiERP_Database_Project/Inventory/Tables/Item_Group.sql
@@ -8,8 +8,8 @@
     [LastModified_Date]   DATE             NOT NULL,
     [Created_By]          BIGINT           NULL,
     [LastModified_By]     BIGINT           NULL,
-    PRIMARY KEY CLUSTERED ([Group_Id] ASC),
-    FOREIGN KEY ([Created_By]) REFERENCES [Hrms].[Employee_Master] ([Employee_Id]),
-    FOREIGN KEY ([LastModified_By]) REFERENCES [Hrms].[Employee_Master] ([Employee_Id])
+    CONSTRAINT [PK_Item_Group_Group_Id] PRIMARY KEY CLUSTERED ([Group_Id] ASC),
+    CONSTRAINT [FK_Created_By_Item_Group_To_Hrms.Employee_Master] FOREIGN KEY ([Created_By]) REFERENCES [Hrms].[Employee_Master] ([Employee_Id]),
+    CONSTRAINT [FK_LastModified_By_Item_Group_To_Hrms.Employee_Master] FOREIGN KEY ([LastModified_By]) REFERENCES [Hrms].[Employee_Master] ([Employee_Id])
 );
 
--- SaiERP_Database_Project/Inventory/Tables/Item_Master.sql
+++ SaiERP_Database_Project/Inventory/Tables/Item_Master.sql
@@ -1,16 +1,16 @@
 ﻿CREATE TABLE [Inventory].[Item_Master] (
     [Item_Master_Id]    UNIQUEIDENTIFIER NOT NULL,
-    [Item_Grp_Id]   UNIQUEIDENTIFIER NOT NULL,
+    [Item_Grp_Id]       UNIQUEIDENTIFIER NOT NULL,
     [Item_Description]  VARCHAR (1000)   NOT NULL,
     [Stock_Unit]        INT              NOT NULL,
     [Unit_Weight]       INT              NOT NULL,
-    [Created_Date]      DATETIME         NOT NULL,
-    [LastModified_Date] DATETIME         NOT NULL,
+    [Created_Date]      DATE             NOT NULL,
+    [LastModified_Date] DATE             NOT NULL,
     [Created_By]        BIGINT           NULL,
     [LastModified_By]   BIGINT           NULL,
-    PRIMARY KEY CLUSTERED ([Item_Master_Id] ASC),
-    FOREIGN KEY ([Created_By]) REFERENCES [Hrms].[Employee_Master] ([Employee_Id]),
-    FOREIGN KEY ([Item_Grp_Id]) REFERENCES [Inventory].[Item_Group] ([Group_Id]),
-    FOREIGN KEY ([LastModified_By]) REFERENCES [Hrms].[Employee_Master] ([Employee_Id])
+    CONSTRAINT [PK_Item_Master_Id] PRIMARY KEY CLUSTERED ([Item_Master_Id] ASC),
+    CONSTRAINT [FK_Created_By_To_Hrms_Employee_Master] FOREIGN KEY ([Created_By]) REFERENCES [Hrms].[Employee_Master] ([Employee_Id]),
+    CONSTRAINT [FK_Item_Grp_Id_To_Item_Master] FOREIGN KEY ([Item_Grp_Id]) REFERENCES [Inventory].[Item_Group] ([Group_Id]),
+    CONSTRAINT [FK_LastModified_By_To_Hrms_Employee_Master] FOREIGN KEY ([LastModified_By]) REFERENCES [Hrms].[Employee_Master] ([Employee_Id])
 );
 
